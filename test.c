7c74d3797bef78d472ffe5df24654eb940c67cd0
spl-clauvio <2386817103@qq.com>
2023-08-31 18:08:52 +0800

    sc

--- test.c
+++ test.c
@@ -2,8 +2,10 @@
 #include<stdlib.h>
 int main()
 {
+    int a=0;
+    scanf("%d",&a);
     printf("hello,world\n你好，世界！\n");
-    printf("%d\n",sizeof(double));
+    printf("%d\n%d\n",sizeof(double),a);
     system("pause");
     return 0;
 }
