d4dfe80f4f9f472ca3e97838ba6f0603d6ef821d
spl-clauvio <2386817103@qq.com>
2023-09-01 09:58:59 +0800

    23/9/1

---
+++ ext.c
@@ -0,0 +1,6 @@
+#include<stdlib.h>
+int d=15;
+void main()
+{
+
+}
--- test.c
+++ test.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+extern int d;
 int main()
 {
-    int a=0;
-    scanf("%d",&a);
+    int a=0,b=0,c=0;
+    scanf("%d %d",&a,&b);
+    c=a+b;
     printf("hello,world\n你好，世界！\n");
-    printf("%d\n%d\n",sizeof(double),a);
+    printf("%d\n%d\n",c,d);
     system("pause");
     return 0;
 }
