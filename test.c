de19d8f889c7ac8ae4bd8465134c81dde5d64a9f
spl-clauvio <2386817103@qq.com>
2023-09-01 10:25:09 +0800

    9/1

--- test.c
+++ test.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-extern int d;
 int main()
 {
     int a=0,b=0,c=0;
     scanf("%d %d",&a,&b);
     c=a+b;
     printf("hello,world\n你好，世界！\n");
-    printf("%d\n%d\n",c,d);
+    printf("%d\n",c);
     system("pause");
     return 0;
 }
